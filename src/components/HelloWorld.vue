242de950e0f94b86fb4d4cdeb97d3abd2929f690
darkbenq3d4 <davidelfuerte2010@gmail.com>
2022-01-18 14:31:12 -0400

    First real commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+<div id="drawflow"></div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import Drawflow from 'drawflow';
+import 'drawflow/dist/drawflow.min.css'
+
+@Options({
+  props: {
+    msg: String,
+  },
+})
+export default class HelloWorld extends Vue {
+  msg!: string;
+  
+  
+
+  mounted() {
+   this.$nextTick(() => {
+    let id = document.getElementById("drawflow") as HTMLElement;
+    console.log(id)
+    const editor = new Drawflow(id, Vue, this);
+    editor.start();
+    
+
+    
+   })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
